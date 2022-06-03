b2b926602f306656b7b3bf512303cd955bb54b07
Ethan Kunis <EPKunis@student.fullsail.edu>
2022-06-03 19:50:47 -0400

    contine from vid

--- cMain.cpp
+++ cMain.cpp
@@ -3,7 +3,7 @@
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "LAB 1 MIN SWEAPER", wxPoint(30,30), wxSize(800, 600))
 {
 	button1 = new wxButton(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150, 50));
-	//text = new wxText(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150, 50));
+	text = new wxText(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150, 50));
 	button1 = new wxButton(this, wxID_ANY, "Click Me", wxPoint(10, 10), wxSize(150, 50));
 }
 
